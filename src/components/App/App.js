54acac7dc80bfb404e7c08aa1978c5af5c547941
Srikanta <busrikanta@gmail.com>
2022-03-27 13:15:29 +0530

    SignIn Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
+import React, { useEffect } from "react";
+
 import MyNav from "../MyNav/MyNav";
 import SignIn from "../SignIn/SignIn";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "BBCCX";
+  }, []);
   return (
     <div>
       <MyNav />
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,25 +3,40 @@ import "./SignIn.css";
 
 const SignIn = () => {
   return (
-    <div className="FlexCenter SignInMain">
-      <span className="SignInLabel SignInColor">Sign In</span>
-      <input
-        type="text"
-        className="pa2 fw4 input-reset br3 SignIn-TextBox w-20 measure white mb4"
-        placeholder="Username"
-        id="Username"
-      ></input>
-      <input
-        type="password"
-        className="pa2 fw4 input-reset br3 SignIn-TextBox w-20 measure white"
-        placeholder="Password"
-        id="Password"
-      ></input>
-      <div className="mt2">
-        <input type="checkbox" id="RememberMe" className="mr2" />
-        <label forHtml="RememberMe" className="SignInColor">
-          Remember Me
-        </label>
+    <div className="FlexCenter MainPage">
+      <div className="FlexCenter SignInMain">
+        <span className="SignInLabel SignInColor">Sign In</span>
+        <input
+          type="text"
+          className="pa2 fw4 input-reset br3 SignIn-TextBox measure white mb4"
+          placeholder="Username"
+          id="Username"
+        ></input>
+        <input
+          type="password"
+          className="pa2 fw4 input-reset br3 SignIn-TextBox measure white"
+          placeholder="Password"
+          id="Password"
+        ></input>
+        <div className="mt3 FlexCenter">
+          <div>
+            <input type="checkbox" id="RememberMe" className="mr2" />
+            <label forHtml="RememberMe" className="SignInColor">
+              Remember Me
+            </label>
+          </div>
+          <div className="ForgotPassword">
+            <label className="SignInColor">Forgot password?</label>
+          </div>
+        </div>
+        <div className="mt3 FlexCenter">
+          <button className="b ph3 pv2 input-reset grow pointer f6 dib SignInButton white br3">
+            SignIn
+          </button>
+          <button className="b ph3 pv2 input-reset grow pointer f6 dib SignInButton white br3 ml4">
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
