409cc6a1e7e868217bfc2a105c123fb1209feb13
finnyta <finnyta@gmail.com>
2022-03-27 14:52:54 +0530

    Update main.js

--- Other-Files/main.js
+++ Other-Files/main.js
@@ -1,59 +1,95 @@
-const SHA256 = require("crypto-js/sha256");
-class Block {
-  constructor(index, timestamp, data, previousHash = "") {
-    this.index = index;
-    this.previousHash = previousHash;
-    this.timestamp = timestamp;
-    this.data = data;
-    this.previousHash = previousHash;
-    this.hash = this.calculateHash();
-  }
-
-  calculateHash() {
-    return SHA256(
-      this.previousHash + this.timestamp + JSON.stringify(this.data)
-    ).toString();
-  }
-}
-
-class Blockchain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-  }
-  createGenesisBlock() {
-    return new Block(0, "01/01/2017", "Genesis block", "0");
-  }
-  getLatestBlock() {
-    return this.chain[this.chain.length - 1];
-  }
-
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
-    this.chain.push(newBlock);
-  }
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash != currentBlock.calculateHash()) {
-        return false;
-      }
-      if (currentBlock.previousHash != previousBlock.hash) {
-        return false;
-      }
+const SHA256 = require('crypto-js/sha256');
+class Block{
+    //This class create blocks
+    constructor(index, data, previousHash = '') {
+        this.index=index;
+        this.previousHash = previousHash;
+        this.timestamp = Date.now();
+        this.data = data;
+        this.dataHash = this.getHash();
+        this.previousHash=previousHash;
+        this.hash=this.calculateHash();
     }
-    return true;
-  }
+ //To create Hash based on all the details
+  calculateHash() {
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+//To create Hash only based on data/content
+    getHash() {
+        return SHA256(JSON.stringify(this.data)).toString();
+    }
+
 }
+
+class Blockchain{
+    constructor(){
+        this.chain=[this.createGenesisBlock()];
+    }
+
+    //To create Genesis Block
+    createGenesisBlock(){
+        return new Block(0,"Genesis Block","0");
+    }
+    //To find the last block
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    //To check the chain is valid or not
+    isChainValid(){
+        for(let i=1;i<this.chain.length; i++){
+            const currentBlock=this.chain[i];
+            const previousBlock=this.chain[i-1];
+            
+            if(currentBlock.hash !=currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash!=previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Compare the New Hash with other block hash
+    //Hash is based on Data/Content
+    isDataHashExist(newDatahash){
+        for(let i=1;i<this.chain.length; i++){
+            const currentBlock=this.chain[i];
+            
+            if(currentBlock.dataHash == newDatahash){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    // To create new block
+    addBlock(newBlock){
+        newBlock.previousHash=this.getLatestBlock().hash;
+        newBlock.hash=newBlock.calculateHash();
+        newBlock.dataHash=newBlock.getHash
+
+        //if (isDataHashExist(newBlock.dataHash) == false) {
+                this.chain.push(newBlock);
+        //    }
+    }
+    
+}
+
+
 
 let objBC = new Blockchain();
-objBC.addBlock(new Block(1, "10/07/2017", { amount: 4 }));
-objBC.addBlock(new Block(2, "10/07/2017", { amount: 10 }));
 
-console.log("Vaild?" + objBC.isChainValid());
+objBC.addBlock(new Block(1,"Here is the Data1"));
+objBC.addBlock(new Block(2,"Here is the Data2"));
+objBC.addBlock(new Block(3,"Here is the Data1"));
 
-objBC.chain[1].data = { amount: 100 };
-console.log("Vaild?" + objBC.isChainValid());
-console.log(JSON.stringify(objBC, null, 4));
+//console.log('Vaild?' + objBC.isChainValid());
+
+console.log(JSON.stringify(objBC,null,2));
+
+
+
+
