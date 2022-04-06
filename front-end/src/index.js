332dfef687197461e7c59246eead77e90bcb23d7
Srikanta B U <busrikanta@gmail.com>
2022-04-06 20:48:29 +0530

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./components/App/App";
 import "./index.css";
 import "tachyons";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <div>
+//       Hello
+//       <App />
+//     </div>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
